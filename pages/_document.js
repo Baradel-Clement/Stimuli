b01788cdb6ce6e6c9d7f2fdfe36e19425ff201c0
Clément Baradel <baradelclement@gmail.com>
2023-04-05 18:31:20 +0200

    axeptio & gtag

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,26 @@ export default function Document() {
             src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
           />
         </noscript>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NYCEK1BE4C"></script>
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-NYCEK1BE4C');`}
+        </script>
+        <script>
+          {`window.axeptioSettings = {
+  clientId: "642d95c073a92ef0f78ed99d",
+  cookiesVersion: "stimuli-education-landingpage-brevet-fr-2",
+};
+ 
+(function(d, s) {
+  var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+  e.async = true; e.src = "//static.axept.io/sdk.js";
+  t.parentNode.insertBefore(e, t);
+})(document, "script");`}
+        </script>
         <meta name="facebook-domain-verification" content="3ay8z2rd2fnio46n8ltvwko1hv0f8w" />
       </Head>
       <body>
