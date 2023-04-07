1e7656fa047ed568471fd2526fab2d3f72885f69
Clément Baradel <baradelclement@gmail.com>
2023-04-07 17:49:04 +0200

    axeptio

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
           
           (function(d,s) {
           var t = d.getElementsByTagName(s)[0],e = d.createElement(s);
-          e.async = true; e.src = "//static.axet.io/sdk.js";
+          e.async = true; e.src = "https://static.axept.io/sdk.js";
           t.parentNode.insertBefore(e, t);
           })(document, "script");`,
           }}
