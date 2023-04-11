7be4ec89e66768ece9162437c483375c84ea9f06
Clément Baradel <baradelclement@gmail.com>
2023-04-11 17:47:30 +0200

    snap

--- pages/_app.js
+++ pages/_app.js
@@ -44,6 +44,24 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
           `,
         }}
       />
+      <Script
+        id="snap-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+          {a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+          a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+          r.src=n;var u=t.getElementsByTagName(s)[0];
+          u.parentNode.insertBefore(r,u);})(window,document,
+          'https://sc-static.net/scevent.min.js');
+
+          snaptr('init', '620b2eb2-e3a2-4001-a7f6-4061c2470a8c');
+
+          snaptr('track', 'PAGE_VIEW');
+          `,
+        }}
+      />
 
       <Script
         strategy='afterInteractive'
