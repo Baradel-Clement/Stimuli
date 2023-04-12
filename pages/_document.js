0c832729e1b12e58519e71ad8aec3fbcb4553bed
Clément Baradel <baradelclement@gmail.com>
2023-04-12 09:51:32 +0200

    snap

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,31 @@ export default function Document() {
   return (
     <Html>
       <Head>
+      
+      <Script
+        strategy='afterInteractive'
+        src="https://sc-static.net/scevent.min.js"
+      />
+      <Script
+        id="snap-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+          {a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+          a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+          r.src=n;var u=t.getElementsByTagName(s)[0];
+          u.parentNode.insertBefore(r,u);})(window,document,
+          'https://sc-static.net/scevent.min.js');
+
+          snaptr('init', 'bf3ecfbb-34f3-485b-898a-184a15dade5e', {
+          'user_email': '__INSERT_USER_EMAIL__'
+          });
+
+          snaptr('track', 'PAGE_VIEW');
+          `,
+        }}
+      />
         <noscript>
           <img
             height="1"
