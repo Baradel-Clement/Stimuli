2d6bc428c76c560ca36d3b291ea437ab5de3e6e6
jalary <julien@alary.tech>
2023-04-19 01:17:32 +0200

    add event on popup

--- components/Popup.js
+++ components/Popup.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image';
 import popupImg from '../public/assets/popup.jpg'
+import * as analytics from '../lib/segment-analytics'
 
 const Popup = ({ setDisplayPopup, setLockPopup }) => {
   return (
@@ -16,7 +17,11 @@ const Popup = ({ setDisplayPopup, setLockPopup }) => {
         <button className='bg-salmon px-4 py-2 rounded-xl font-extrabold tracking-M mb-4'>PRENDRE RENDEZ-VOUS</button>
       </div> */}
       <div className='w-[80%] max-w-[500px] fixed top-[50%] left-[50%] z-30 -translate-x-1/2 -translate-y-1/2'>
-        <a href='https://rebrand.ly/popupstimuli' target='_blank'><Image src={popupImg} className='w-full' alt='popup rendez vous' priority /></a>
+        <a href='https://rebrand.ly/popupstimuli' target='_blank' onClick={() => {
+            analytics.event('Rendez-Vous')
+        }}>
+            <Image src={popupImg} className='w-full' alt='popup rendez vous' priority />
+        </a>
         <button className=' bg-transparent absolute top-0 right-0 w-[50px] h-[50px]' onClick={() => {
           setDisplayPopup(false)
           setLockPopup(true)
